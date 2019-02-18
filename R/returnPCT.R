35e936466e26140c984bc6f4b6ce08e1cf03d2de
John D. Johnson <jdjohn215@gmail.com>
2019-02-18 10:22:48 -0600

    add mlsp()

---
+++ R/returnPCT.R
@@ -0,0 +1,52 @@
+#' Return a rounded marginal
+#'
+#' \code{mlsp} returns an integer value for a marginal. It's the percent of valid respondents
+#' giving the specified response.
+#'
+#' @param mulaw The data.frame containing the version of the integrated file you wish to use
+#' @param variable The unquoted variable name
+#' @param response The desired response. It can be either numeric or the value label.
+#'
+#' @return A dataframe.
+#' @export
+#' @import dplyr
+#' @import stringr
+#' @examples
+#' mlsp(mulaw = df, variable = g40, response = 1)
+#' mlsp(mulaw = df, variable = g40, response = "approve)
+#'
+
+mlsp <- function(mulaw, variable, response){
+  variable <- enquo(variable)
+
+  # Do this if response is number
+  if(suppressWarnings(!is.na(as.numeric(response)) == TRUE)){
+    d.temp <- mulaw %>%
+      filter(!is.na(!!variable)) %>%
+      mutate(total = sum(zwave_weight)) %>%
+      filter(!!variable == response) %>%
+      summarise(pct = sum(zwave_weight)/first(total))
+  }
+
+  # Do this if response is value label
+  if(suppressWarnings(is.na(as.numeric(response)) == TRUE)){
+
+    # reformat response
+    response = response %>%
+      stringr::str_to_upper() %>%
+      stringr::str_replace_all("[[:punct:]]", " ") %>%
+      stringr::str_squish()
+
+    d.temp <- mulaw %>%
+      filter(!is.na(!!variable)) %>%
+      mutate(total = sum(zwave_weight),
+             !!variable := labelled::to_character(!!variable),
+             !!variable := stringr::str_to_upper(!!variable),
+             !!variable := stringr::str_replace_all(!!variable, "[[:punct:]]", " "),
+             !!variable := stringr::str_squish(!!variable)) %>%
+      filter(!!variable == response) %>%
+      summarise(pct = sum(zwave_weight)/first(total))
+  }
+
+  round((d.temp$pct)*100, digits = 0)
+}
