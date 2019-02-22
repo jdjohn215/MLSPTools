bc8106cdacf10178c1617fa58cf606e4fd38232a
John D. Johnson <jdjohn215@gmail.com>
2019-02-22 16:07:48 -0600

    add print.word function

---
+++ R/print.word.R
@@ -0,0 +1,42 @@
+#' Print a number as an english word
+#'
+#' \code{print.word} a character string of a number
+#'
+#'  This function is a wrapper for either the words() or Words() function from
+#'  the english package. It defaults to upper case, but lower case can
+#'  be set using case = lower. print.word() also adds a hyphen between words.
+#'
+#' @param number any numeric value
+#' @param case either "upper" or "lower." Defaults to "upper."
+#'
+#' @return A character string
+#' @export
+#' @importFrom stringr str_to_lower
+#' @importFrom stringr str_sub
+#' @importFrom english words
+#' @importFrom english Words
+#'
+#' @examples
+#' print.word(42)
+#' print.word(42, case = "lower)
+
+print.word <- function(number, case){
+  # set default case to upper
+  if(missing(case)){
+    case = "upper"
+  }
+
+  if(stringr::str_to_lower(case) == "upper"){
+    output <- stringr::str_replace(
+      english::Words(number),
+      " ", "-")
+  }
+
+  if(stringr::str_to_lower(case) == "lower"){
+    output <- stringr::str_replace(
+      english::words(number),
+      " ", "-")
+  }
+
+  output
+}
