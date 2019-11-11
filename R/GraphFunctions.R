fed26bca2e779672f6a210421cd5624ed07d406e
John D. Johnson <jdjohn215@gmail.com>
2019-11-11 13:17:49 -0600

    add time series scatter plot

--- R/GraphFunctions.R
+++ R/GraphFunctions.R
@@ -154,3 +154,87 @@ mlspCrosstabBar <- function(crosstabtable, titlevar = NULL, title = NULL, subtit
 
   p
 }
+
+################################################################################################
+#' Make a time series scatterplot
+#'
+#' \code{mlspTimeSeriesScatter} creates a scatter plot with time on the x-axis
+#'
+#'  This function takes a topline table created with make.topline() and returns it as
+#'  a bar chart.
+#'
+#' @param timeseriestable The source table created by make.ts()
+#' @param titlevar A quoted variable name. If supplied, the full text of this variable will be the title
+#' @param title A character string. If supplied it will be the graph's title.
+#' @param subtitle If supplied, it will be the graph's subtitle
+#' @param theme Optional. One of either "LubarSlides" or "MLSP". This sets the theme to
+#' theme_LubarSlides or theme_MLSP, respectively.
+#' @param PlotMargins Only applicable when theme = "LubarSlides". It allows the user to manually change the plot.margin parameter.
+#' @param wraptitle = the length at which to wrap the character title string. By default
+#' it is 95 which matches the width of a LubarSlide.
+#'
+#' @return A dataframe.
+#' @export
+#' @import dplyr
+#' @import stringr
+#' @import ggplot2
+#'
+#' @examples
+#' make.ts(g40, integ) %>% mlspTimeSeriesScatter()
+
+mlspTimeSeriesScatter <- function(timeseriestable, titlevar = NULL, title = NULL, subtitle = NULL,
+                            theme = "default", LubarSlides = TRUE,
+                            PlotMargins = c(0.25, 0, 2, 1),
+                            wraptitle = 95, legendPosition = "top",
+                            legendJust = "right"){
+  title.text <- "no title provided"
+  if(!is.null(titlevar)){
+    qs <- suppressMessages(readxl::read_excel("~/Dropbox/MuLawPoll1/IntegCurrentVariables.xlsx",
+                                              sheet = 2)) %>%
+      filter(!is.na(Q)) %>%
+      select(variable = 1, question = 7)
+    title.text <- qs$question[qs$variable == titlevar]
+  } else if(!is.null(title)){
+    title.text = title
+  }
+
+  if(names(timeseriestable[3]) == "pct"){
+    timeseriestable <- timeseriestable %>%
+      rename(PollDate = 1, yvar = 2)
+  } else if(names(timeseriestable[3]) != "pct"){
+    timeseriestable <- timeseriestable %>%
+      rename(PollDate = 1) %>%
+      pivot_longer(cols = -PollDate, names_to = "yvar", values_to = "pct")
+  }
+
+  p <- timeseriestable %>%
+    ggplot(aes(as.Date(PollDate), pct, color = yvar)) +
+    geom_point() +
+    scale_x_date(name = NULL) +
+    scale_y_continuous(name = NULL, limits = c(0,100),
+                       labels = scales::percent_format(scale = 1, accuracy = 1),
+                       expand = c(0,0.01)) +
+    labs(title = str_wrap(title.text, width = wraptitle),
+         subtitle = subtitle)
+
+  if(theme == "LubarSlides"){
+    p <- p +
+      theme_LubarSlides(PlotMargins = PlotMargins) +
+      theme(legend.position = legendPosition,
+            legend.justification = legendJust,
+            legend.title = element_blank())
+  } else if(theme == "MLSP"){
+    p <- p +
+      theme_MLSP() +
+      theme(legend.position = legendPosition,
+            legend.justification = legendJust,
+            legend.title = element_blank())
+  } else{
+    p <- p +
+      theme(legend.position = legendPosition,
+            legend.justification = legendJust,
+            legend.title = element_blank())
+  }
+
+  p
+}
