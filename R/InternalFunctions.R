0eb06d24ec03d3a60d82c30e22e2b34a365a6141
John D. Johnson <jdjohn215@gmail.com>
2019-11-15 12:53:30 -0600

    init and add internal graphing palette functions

---
+++ R/InternalFunctions.R
@@ -0,0 +1,65 @@
+# These functions are called internally by MLSPTools.
+# They cannot be accessed by a user who has simply loaded the package.
+
+####################################################################################
+# These functions are used to choose palettes in the mlspgraph functions
+####################################################################################
+
+# This function determines the type of input table
+table.type <- function(table){
+  if(names(table)[1] == "Response"){
+    tabletype <- "topline"
+  } else if(names(table)[1] == "PollDate"){
+    tabletype <- "ts"
+  } else {
+    tabletype <- "crosstab"
+  }
+  tabletype
+}
+
+# based on the type of input table, this function extracts the values to which fill colors will be assigned
+color.levels <- function(table){
+  if(table.type(table) == "topline"){
+    colorlevels <- table %>% mutate(Response = as.character(Response)) %>% pull(Response)
+  } else if(table.type(table) == "ts"){
+    colorlevels <- names(table)[2:ncol(table)]
+  } else if(table.type(table) == "crosstab"){
+    colorlevels <- names(table)[2:(ncol(table)-1)]
+  }
+  colorlevels
+}
+
+# this function uses the other two functions to assign a palette to the fill values
+guess.palette <- function(table, fillPalette = "guess"){
+  colorlevels <- color.levels(table) %>%
+    str_to_lower()
+
+  if(fillPalette == "guess"){
+    # Check if it's a party ID variable
+    if(colorlevels[1] == "republican"){
+      # check if 3 category
+      if(colorlevels[2] == "independent"){
+        mlspPalette <- pid3.palette
+      } else {
+        mlspPalette <- pid5.palette
+      }
+      #this covers 3-cat support/oppose question
+    } else if(colorlevels[1] %in% c("approve", "support", "favor")){
+      mlspPalette <- fav2.palette
+    } else if(colorlevels[1] %in% c("strongly approve", "strongly support", "strongly favor")){
+      mlspPalette <- fav4.palette
+    }
+    # add default palette if none of the above conditions are satisfied
+    else {
+      mlspPalette <- RColorBrewer::brewer.pal(n = length(colorlevels), name = "Dark2")
+    }
+    # this next bit checks if the fillPalette is a supplied vector of colors
+  } else if(str_detect(fillPalette, "#")) {
+    mlspPalette <- fillPalette
+    # this checks if fillPalette is the name of an RColorBrewer palette
+  } else if(fillPalette %in% rownames(RColorBrewer::brewer.pal.info)) {
+    mlspPalette <- RColorBrewer::brewer.pal(n = length(colorlevels), name = fillPalette)
+  }
+
+  mlspPalette
+}
