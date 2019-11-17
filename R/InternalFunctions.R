67e92b86369e673e005f89f3641fb2e5b38b7b85
John D. Johnson <jdjohn215@gmail.com>
2019-11-17 15:24:03 -0600

    add palettes

--- R/InternalFunctions.R
+++ R/InternalFunctions.R
@@ -40,11 +40,21 @@ guess.palette <- function(table, fillPalette = "guess"){
   fav2.palette <- c("#238b45", "#6a51a3", "#ff7f00", "#e41a1c", "#fb9a99")
   fav4.palette <- c("#006d2c", "#41ab5d", "#807dba", "#54278f", "#ff7f00", "#e41a1c", "#fb9a99")
 
+  # this palette is for races with a dem and republican in that order
+  voteDR.palette <- c("#74add1", "#f46d43", "#7fc97f", "#beaed4", "#fdc086", "#ffff99")
+
+  # this palette is for races with a rep and dem in that order
+  voteRD.palette <- c("#f46d43", "#74add1", "#7fc97f", "#beaed4", "#fdc086", "#ffff99")
+
+  # this palette is for 4-category sequences, where category 1 is darkest
+  sequence4 <- c("#006d2c", "#31a354", "#74c476", "#a1d99b", "#fdc086", "#beaed4", "#ffff99")
+  sequence3 <- c("#006d2c", "#31a354", "#74c476", "#fdc086", "#beaed4", "#ffff99")
+
   if(fillPalette == "guess"){
     # Check if it's a party ID variable
-    if(colorlevels[1] == "republican"){
+    if(colorlevels[1] %in% c("republican", "rep")){
       # check if 3 category
-      if(colorlevels[2] == "independent"){
+      if(colorlevels[2] %in% c("independent", "ind")){
         mlspPalette <- pid3.palette
       } else {
         mlspPalette <- pid5.palette
@@ -54,6 +64,17 @@ guess.palette <- function(table, fillPalette = "guess"){
       mlspPalette <- fav2.palette
     } else if(colorlevels[1] %in% c("strongly approve", "strongly support", "strongly favor", "strongly favorable")){
       mlspPalette <- fav4.palette
+      # this covers head-to-heads against Trump
+    } else if(colorlevels[2] == "donald trump") {
+      mlspPalette <- voteDR.palette
+    } else if(colorlevels[1] == "donald trump") {
+      mlspPalette <- voteRD.palette
+    }
+    # This covers 4-cat sequential responses
+    else if(word(colorlevels[1]) == "very" & word(colorlevels[4], 1, 3) == "not at all") {
+      mlspPalette <- "sequence4"
+    } else if(colorlevels[1] == "living comfortably") {
+      mlspPalette <- sequence3
     }
     # assign fav2 if only 4 response categories
     else if(length(colorlevels) < 5) {
@@ -79,6 +100,10 @@ guess.palette <- function(table, fillPalette = "guess"){
       mlspPalette <- fav4.palette
     } else if(fillPalette == "fav2") {
       mlspPalette <- fav2.palette
+    } else if(fillPalette == "voteDR"){
+      mlspPalette <- voteDR.palette
+    } else if(fillPalette == "voteRD"){
+      mlspPalette <- voteRD.palette
     }
   }
 
