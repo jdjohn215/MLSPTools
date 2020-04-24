e7f21b25b2da52df09661d61b6f97b4bf0ec8040
John D. Johnson <jdjohn215@gmail.com>
2020-04-24 14:54:37 -0500

    replace moe.crosstab.3way with moe_crosstab_3way

--- R/moeCrosstab3Way.R
+++ R/moeCrosstab3Way.R
@@ -1,95 +1,34 @@
 #' Make a 3-way crosstab of a variable
 #'
-#' \code{moe.crosstab3way} returns a data.frame of a crosstab of two variables by a 3rd variable
+#' \code{moe_crosstab_3way} returns a data.frame of a crosstab of two variables by a 3rd variable
 #'  along with margins of error
 #'
-#'  This function returns a crosstab by a 3rd variable of two supplied variables along with margins of error.
-#'  The resulting data.frame defaults
-#'  to long format, but can be in wide format if format = "wide" is specified.
+#'  This is a wrapper around pollster::moe_crosstab_3way
 #'
-#' @param x The grouping variable
-#' @param y the dependent variable
-#' @param z the 2nd control variable.
 #' @param mulaw The data.frame containing the version of the integrated file you wish to use
-#' @param weights The weight variable, defaults to zwave_weight
-#' @param format One of "wide" or "long", defaults to "long"
+#' @param x The independent variable
+#' @param y The dependent variable
+#' @param z The third variable
+#' @param weight The weighting variable, defaults to zwave_weight
+#' @param remove An optional character vector of values to remove from final table (e.g. DK/Ref).
+#' This will not affect any calculations made. The vector is not case-sensitive.
+#' @param n Logical. If TRUE add a row total column
+#' @param pct_type one of "row" or "cell"
+#' @param format one of "wide" or "long
+#' @param zscore defaults to 1.96, consistent with a 95% confidence interval
 #'
-#' @return A dataframe.
+#' @return A tibble
 #' @export
-#' @import dplyr
-#' @import stringr
-#' @importFrom labelled to_factor
-#' @importFrom tidyr pivot_wider
 #'
 #' @examples
-#' moe.crosstab.wave(zpid3, g40, integ, format = "wide")
+#' moe_crosstab_3way(integ, zpid3, g40, ac7)
 #'
-moe.crosstab3way <- function(x, y, z, mulaw, weights = zwave_weight, format = "long"){
-  # calculate design effect for sample comprised of all waves question is present for
-  valid.waves <- mulaw %>%
-    filter(!is.na({{x}}),
-           !is.na({{y}}),
-           !is.na({{z}})) %>%
-    mutate(zwave = remove_labels(zwave)) %>%
-    group_by(zwave) %>%
-    summarise() %>%
-    pull()
 
-  # this data.frame has the complete waves in which the variables was asked
-  d.deff <- mulaw %>%
-    filter(zwave %in% valid.waves) %>%
-    rename(weights = {{weights}})
+moe_crosstab_3way <- function(mulaw, x, y, z, weight = zwave_weight,
+                          remove = c(""), n = TRUE, pct_type = "row",
+                          format = "long", zscore = 1.96){
 
-  deff <- deff_calc(d.deff$weights)
-
-  output <- mulaw %>%
-    filter(!is.na({{x}}),
-           !is.na({{y}}),
-           !is.na({{z}})) %>%
-    mutate({{z}} := to_factor({{z}}),
-           {{x}} := to_factor({{x}}),
-           {{y}} := to_factor({{y}})) %>%
-    group_by({{z}}, {{x}}) %>%
-    mutate(total = sum({{weights}}),
-           n.row = length({{weights}})) %>%
-    group_by({{z}}, {{x}}, {{y}}) %>%
-    summarise(frequency = sum({{weights}}),
-              n.row = first(n.row),
-              pct = frequency/first(total)) %>%
-    ungroup() %>%
-    mutate(moe = moedeff_calc(pct = pct, deff = deff, n = n.row),
-           pct = pct*100) %>%
-    select({{z}}, {{x}}, {{y}}, frequency, pct, moe)
-
-
-  # make wide if needed
-  if(format == "wide"){
-    output.wide <- output %>%
-      select(-frequency) %>%
-      pivot_wider(names_from = {{y}}, values_from = c("pct", "moe"))
-
-    # fix names
-    y.order <- output %>%
-      select({{y}}) %>%
-      pull() %>%
-      levels()
-
-    output.names <- names(output.wide)[3:length(names(output.wide))] %>%
-      tibble() %>%
-      rename(oldNames = 1) %>%
-      mutate(order = (1:n())+2,
-             name = str_sub(oldNames, 5, -1),
-             suffix = str_sub(oldNames, 1, 3),
-             name = factor(name, levels = y.order)) %>%
-      arrange(name) %>%
-      mutate(fixname = paste(name, suffix, sep = "_"))
-
-    output.wide <- output.wide %>%
-      select(1, 2, output.names$order)
-    names(output.wide)[3:length(names(output.wide))] <- output.names$fixname
-
-    output <- output.wide
-  }
-
-  output
+  pollster::moe_crosstab_3way(df = mulaw, x = {{x}}, y = {{y}}, z = {{z}}, weight = {{weight}},
+                          remove = remove, n = n, pct_type = pct_type,
+                          format = format, zscore = zscore)
 }
