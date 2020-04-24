68ae324d37e37b4af729a167103d5ada7bc24e80
John D. Johnson <jdjohn215@gmail.com>
2020-04-24 14:16:08 -0500

    replace make.crosstab.3way with crosstab_3way()

--- R/MakeCrosstab3Way.R
+++ R/MakeCrosstab3Way.R
@@ -1,6 +1,6 @@
 #' Make a weighted crosstab of row proportions, by wave
 #'
-#' \code{make.crosstab.3way} returns a data.frame containing a weighted crosstab of two variables
+#' \code{crosstab_3way} returns a data.frame containing a weighted crosstab of two variables
 #' by wave.
 #'
 #'  This function returns a crosstab by wave. The resulting data.frame is in long format, suitable
@@ -24,53 +24,14 @@
 #' @importFrom tidyr pivot_wider
 #'
 #' @examples
-#' make.crosstab.wave(x = zpid3, y = g40v2, mulaw = df, remove = c("don't know", "refused"))
+#' crosstab_3way(integ, zpid3, g40, ac7)
 #'
-#' # Now make that output a faceted graph
-#' make.crosstab.wave(x = zpid3, y = g40v2,
-#'                    mulaw = df, remove = c("don't know", "refused")) %>%
-#'      ggplot(aes(zpid3, ae2, col = ae2)) +
-#'        geom_point() +
-#'        geom_smoth() +
-#'        facet_wrap(facets = vars(zpid3))
 
-make.crosstab.3way <- function(x, y, z, mulaw, remove,
-                               weight = zwave_weight,
-                               n = TRUE){
+crosstab_3way <- function(mulaw, x, y, z, weight = zwave_weight,
+                               remove = c(""), n = TRUE, pct_type = "row",
+                               format = "wide"){
 
-  # if remove is missing replace with empty string
-  if(missing(remove)){
-    remove = ""
-  }
-
-  # Make
-  d.output <- mulaw %>%
-    # Remove missing cases
-    filter(!is.na({{x}}),
-           !is.na({{y}}),
-           !is.na({{z}})) %>%
-    # Convert to ordered factors
-    mutate({{x}} := to_factor({{x}}, sort_levels = "values"),
-           {{y}} := to_factor({{y}}, sort_levels = "values"),
-           {{z}} := to_factor({{z}}, sort_levels = "values")) %>%
-    # Calculate denominator
-    group_by({{x}}, {{z}}) %>%
-    mutate(total = sum({{weight}})) %>%
-    # Calculate proportions
-    group_by({{x}}, {{y}}, {{z}}) %>%
-    summarise(pct = sum({{weight}})/first(total),
-              n = first(total)) %>%
-    # Remove values included in "remove" string
-    filter(!str_to_upper({{x}}) %in% str_to_upper(remove),
-           !str_to_upper({{y}}) %in% str_to_upper(remove),
-           !str_to_upper({{z}}) %in% str_to_upper(remove)) %>%
-    ungroup() %>%
-    pivot_wider(names_from = {{y}}, values_from = pct,
-                values_fill = list(pct = 0))
-
-  if(n == FALSE){
-    d.output <- select(d.output, -n)
-  }
-
-  d.output
+  pollster::crosstab_3way(df = mulaw, x = {{x}}, y = {{y}}, z = {{z}}, weight = {{weight}},
+                          remove = remove, n = n, pct_type = pct_type,
+                          format = format)
 }
