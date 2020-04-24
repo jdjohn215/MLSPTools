da3951c95367d53f5626d49c92b607fa7c5cd02f
John D. Johnson <jdjohn215@gmail.com>
2020-04-24 17:24:55 -0500

    convert moe.topline to moe_topline

--- R/moeTopline.R
+++ R/moeTopline.R
@@ -1,52 +1,29 @@
 #' Make a topline of a variable
 #'
-#' \code{moe.topline} returns a data.frame containing a topline table with the margin of error
+#' \code{moe_topline} returns a data.frame containing a topline table with the margin of error
 #'
-#'  This function returns the topline statistics of any given variable.
+#'  This is a wrapper around pollster::moe_topline()
 #'
-#' @param x The variable, unquoted
 #' @param mulaw The data.frame containing the version of the integrated file you wish to use
-#' @param weights The weight variable, defaults to zwave_weight
+#' @param variable The variable, unquoted
+#' @param weight The weight variable, defaults to zwave_weight
+#' @param remove a character string of response values to remove from the output after performing all calculations
+#' @param n Logical, determines if a row total is included or not
+#' @param pct logical, determines if the pct column is included or not
+#' @param valid_pct logical, determines if the valid percent column is included
+#' @param cum_pct Logical, determines if a cumulative percent column is included or not
+#' @param zscore defaults to 1.96, consistent with a 95% confidence interval
 #'
-#' @return A dataframe.
+#' @return A tibble
 #' @export
-#' @import dplyr
-#' @import stringr
-#' @importFrom labelled to_factor
-#' @importFrom tidyr spread
 #'
 #' @examples
-#' make.ts(variable = g2, mulaw = orig, remove = c("refused"), format = "long")
+#' moe_topline(integ, g40)
 #'
-moe.topline <- function(x, mulaw, weights = zwave_weight){
-  # calculate design effect for sample comprised of all waves question is present for
-  valid.waves <- mulaw %>%
-    filter(!is.na({{x}})) %>%
-    mutate(zwave = remove_labels(zwave)) %>%
-    group_by(zwave) %>%
-    summarise() %>%
-    pull()
-
-  # this data.frame has the complete waves in which the variables was asked
-  d.deff <- mulaw %>%
-    filter(zwave %in% valid.waves) %>%
-    rename(weights = {{weights}})
-
-  deff <- deff_calc(d.deff$weights)
-  unweighted.n <- d.deff %>%
-    filter(!is.na({{x}})) %>%
-    nrow()
-
-  output <- mulaw %>%
-    filter(!is.na({{x}})) %>%
-    mutate({{x}} := to_factor({{x}}),
-           total = sum({{weights}})) %>%
-    group_by({{x}}) %>%
-    summarise(frequency = sum({{weights}}),
-              pct = (frequency/first(total))) %>%
-    ungroup() %>%
-    mutate(moe = sqrt(deff)*1.96*sqrt((pct*(1-pct))/(unweighted.n - 1))*100) %>%
-    mutate(pct = pct*100)
-
-  output
+moe_topline <- function(mulaw, variable, weight = zwave_weight,
+                    remove = c(""), n = TRUE, pct = TRUE,
+                    valid_pct = TRUE, cum_pct = TRUE, zscore = 1.96){
+  pollster::moe_topline(df = mulaw, variable = {{variable}}, weight = {{weight}},
+                    remove = remove, n = n, pct = pct, valid_pct = valid_pct,
+                    cum_pct = cum_pct, zscore = zscore)
 }
